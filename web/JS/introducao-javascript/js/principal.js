3d0773dce5431a1f710fbe3292592c5831905af0
FernandoiMac <houspiller@gmail.com>
2017-11-12 16:37:32 -0200

    Commit inicial

---
+++ web/JS/introducao-javascript/js/principal.js
@@ -0,0 +1,29 @@
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0 ; i < pacientes.length; i++) {
+  var trPaciente = pacientes[i];
+
+  var tdPeso = trPaciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
+
+  //Buscando Altura
+  var tdAltura = trPaciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
+
+  //Buscando e calculando o IMC
+  var tdImc = trPaciente.querySelector(".info-imc");
+
+  var imc = peso / (altura * altura);
+  tdImc.textContent = imc.toFixed(2);
+
+  if (peso <= 0 || peso >= 200) {
+    tdImc.textContent = "Peso Inválido!";
+    trPaciente.classList.add("paciente-invalido")
+    }
+
+  if (altura <= 0 || altura >= 3.00) {
+    tdImc.textContent = "Altura Inválida!";
+    trPaciente.style.backgroundColor = "lightcoral";
+  }
+}
